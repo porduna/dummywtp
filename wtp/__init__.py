29399d8ad47e751541c5ddee2d2323c085481ded
Pablo Orduña <pablo.orduna@deusto.es>
2014-10-10 19:11:22 +0200

    Fix the pagination

--- wtp/__init__.py
+++ wtp/__init__.py
@@ -8,23 +8,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+LIMIT = 10
+
 @app.route("/petitions/")
 def browse():
-    args = { 'limit' : 10 }
-    url = "https://api.whitehouse.gov/v1/petitions.json?limit=%(limit)s" % args
+    page = (request.args.get('p') or '1')
+    try:
+        page = int(page)
+    except:
+        page = 1
 
-    if request.args.get('before'):
-        url += '?createdBefore=%s' % int(request.args.get('before'))
+    offset = (page - 1) * LIMIT 
 
-    if request.args.get('after'):
-        url += '?createdAfter=%s' % int(request.args.get('after'))
-
-    print url
+    args = { 'limit' : LIMIT, 'offset' : offset}
+    url = "https://api.whitehouse.gov/v1/petitions.json?limit=%(limit)s&offset=%(offset)s" % args
 
     contents = requests.get(url).json()
     results = contents['results']
-    created = [ result['created'] for result in results ]
-    oldest_new = min(created)
-    newest_new = max(created)
-    return render_template("browse.html", results = results, before = oldest_new, after = newest_new)
+
+    return render_template("browse.html", results = results, page = page)
 
